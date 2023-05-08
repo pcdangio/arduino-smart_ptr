0f36574daa5061d770470a95da36b165c1adec61
Paul D'Angio <pcdangio@gmail.com>
2023-05-07 20:35:47 -0400

    package: set up library

---
+++ src/smart_ptr.hpp
@@ -0,0 +1,9 @@
+/// \file smart_ptr.hpp
+/// \brief The primary header for the smart_ptr library.
+#ifndef SMART_PTR_H
+#define SMART_PTR_H
+
+#include <shared_ptr.hpp>
+#include <unique_ptr.hpp>
+
+#endif
